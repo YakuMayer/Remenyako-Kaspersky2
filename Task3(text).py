b26216ec06a30c9532a6c7d1e8e6bbfcc56498dd
YakuMayer <100630625+YakuMayer@users.noreply.github.com>
2022-06-07 00:59:39 +0300

    Create Task3(text).py

---
+++ Task3(text).py
@@ -0,0 +1,7 @@
+def wabbit (a2,a1):#line:1
+    R1 =[1 ,0 ]#line:2
+    a3 =1 #line:3
+    while a3 <a2:#line:4
+        R1 .insert (0 ,sum (R1 [1 :a1]))#line:5
+        a3 +=1 #line:6
+    print (sum (R1 [0 :a1]))
