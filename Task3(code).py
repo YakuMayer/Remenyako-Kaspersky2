baf5ef2a73307121b4f2f085cd8f20c22a6430c2
YakuMayer <100630625+YakuMayer@users.noreply.github.com>
2022-06-07 19:25:52 +0300

    Update Task3(code).py

--- Task3(code).py
+++ Task3(code).py
@@ -1,6 +1,8 @@
 file = input("Введите название файла: ")
 with open(file, "r") as f:
     text = f.read()
+text2 = text
+text = text.replace(" ", "")
 n = 0
 eq = 0
 srez = []
@@ -9,40 +11,39 @@ names = []
 massive = []
 arguments = (text[text.find("(") + 1:text.find(")")].split(","))
 for i in range(len(text)):
-    if text[i] == "\t" or text[i] == "\n":
+    if text[i] == "\n":
         n = i
     if text[i] == "=":
-        if text[i - 5] == "0" or text[i - 5] == "O":
-            eq = i
-            srez.append(n)
-            srez.append(eq)
+        eq = i
+        srez.append(n)
+        srez.append(eq)
 n = 0
 eq = 0
 for i in range(len(text)):
-    if text[i] == "\t" or text[i] == "\n":
+    if text[i] == "\n":
         n = i
     if text[i] == "[":
-        if "=" in text[i-5:i]:
+        if "=" in text[i - 5:i]:
             eq = i
             srez1.append(n)
             srez1.append(eq)
 for i in range(0, len(srez), 2):
     if i + 1 < len(srez):
         if text[srez[i + 1] + 1] != "[":
-            names.append(text[srez[i] + 1:srez[i + 1]].replace(" ",""))
+            names.append(text[srez[i] + 1:srez[i + 1]].replace(" ", ""))
 for i in range(0, len(srez1), 2):
     if i + 1 < len(srez1):
-        massive.append(text[srez1[i] + 1:srez1[i + 1] - 1].replace(" ",""))
+        massive.append(text[srez1[i] + 1:srez1[i + 1] - 1].replace(" ", ""))
 arguments = list(set(arguments))
 massive = list(set(massive))
 for i in range(len(arguments)):
     name = "a" + str(i + 1)
-    text = text.replace(arguments[i], name)
-for i in range(len(arguments), len(arguments)+len(names)):
+    text2 = text2.replace(arguments[i], name)
+for i in range(len(arguments), len(arguments) + len(names)):
     name = "a" + str(i + 1)
     for j in range(len(names)):
-      text = text.replace(names[j], name)
+        text2 = text2.replace(names[j], name)
 for i in range(len(massive)):
     name1 = "R" + str(i + 1)
-    text = text.replace(massive[i], name1)
-print(text)
+    text2 = text2.replace(massive[i], name1)
+print(text2)
